c3655b093b6102553ecbb2c5ab4dcd1b61050535
capricorn <8250820+capricorn@users.noreply.github.com>
2024-08-19 14:37:31 -0600

    Initial Commit

---
+++ LibBoulder/LibBoulderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibBoulderApp.swift
+//  LibBoulder
+//
+//  Created by Collin Palmer on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LibBoulderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
