5b9d96bb0c1d34d7b4335e37b3c1874554923076
capricorn <8250820+capricorn@users.noreply.github.com>
2024-08-20 14:09:34 -0600

    Implement basic login API

--- LibBoulder/ContentView.swift
+++ LibBoulder/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var loggedIn = false
+    @State private var libraryCardNo: String = ""
+    
+    let api = LibCatAPI()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // TODO: Initial load, determine if cookie exists. If so, try.
+        // Otherwise on 401 a logout will occur.
+        if loggedIn {
+            Button("Fetch books") {
+                
+            }
+        } else {
+            VStack(alignment: .center) {
+                TextField("Library Card Number", text: $libraryCardNo)
+                    .keyboardType(.numberPad)
+                    .monospaced()
+                Button("Log in") {
+                    Task {
+                        do {
+                            try await api.login(cardNumber: libraryCardNo)
+                            loggedIn = true
+                        } catch {
+                            print("Login failed: \(error)")
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ LibBoulder/LibCatAPI.swift
@@ -0,0 +1,29 @@
+//
+//  LibCatAPI.swift
+//  LibBoulder
+//
+//  Created by Collin Palmer on 8/19/24.
+//
+
+import Foundation
+
+class LibCatAPI {
+    static let baseURL = URL(string: "https://libcat-597bd308b192.herokuapp.com")!
+    
+    func login(cardNumber: String) async throws {
+        var req = URLRequest(url: LibCatAPI.baseURL.appending(component: "login"))
+        let jsonBody = [
+            "library_card_no": cardNumber
+        ]
+        
+        req.httpBody = try JSONSerialization.data(withJSONObject: jsonBody)
+        req.httpMethod = "POST"
+        
+        let (data, resp) = try await URLSession.shared.data(for: req)
+        
+        //URLSession.shared.configuration.httpCookieStorage.
+        for cookie in (URLSession.shared.configuration.httpCookieStorage?.cookies ?? []) {
+            print("Cookie: \(cookie.name)")
+        }
+    }
+}
