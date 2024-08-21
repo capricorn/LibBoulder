f99e1191226e8e2dc5f7e369d73dda0ddc511027
capricorn <8250820+capricorn@users.noreply.github.com>
2024-08-21 11:17:35 -0600

    Convert iso8601 to Date

--- LibBoulder/API/Models/CheckedOutBookModel.swift
+++ LibBoulder/API/Models/CheckedOutBookModel.swift
@@ -11,7 +11,7 @@ class CheckedOutBookModel: Codable, Identifiable {
     let title: String
     let author: String
     let renewCount: Int
-    let dueDate: String
+    let dueDate: Date
     let id = UUID()
     
     enum CodingKeys: String, CodingKey {
@@ -19,5 +19,29 @@ class CheckedOutBookModel: Codable, Identifiable {
         case author
         case renewCount = "renew_count"
         case dueDate = "due_date"
+    }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.author = try container.decode(String.self, forKey: .author)
+        self.renewCount = try container.decode(Int.self, forKey: .renewCount)
+        let dueDateISO8601 = try container.decode(String.self, forKey: .dueDate)
+        
+        let dueDate = ISO8601DateFormatter().date(from: dueDateISO8601)
+        guard let dueDate else {
+            throw DecodingError.dataCorrupted(.init(codingPath: [CodingKeys.dueDate], debugDescription: "iso8601 incorrectly formatted: \(dueDateISO8601)"))
+        }
+        
+        self.dueDate = dueDate
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.title, forKey: .title)
+        try container.encode(self.author, forKey: .author)
+        try container.encode(self.renewCount, forKey: .renewCount)
+        try container.encode(self.dueDate.formatted(.iso8601), forKey: .dueDate)
     }
 }
